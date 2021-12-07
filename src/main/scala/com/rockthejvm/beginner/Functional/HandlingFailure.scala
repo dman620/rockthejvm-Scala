d642982963b0416e0cef54da3d5451a11eb24120
Derek Mandl <homeses@gmail.com>
2021-12-07 15:38:11 -0600

    handling failure mostly completed

--- src/main/scala/com/rockthejvm/beginner/Functional/HandlingFailure.scala
+++ src/main/scala/com/rockthejvm/beginner/Functional/HandlingFailure.scala
@@ -1,5 +1,71 @@
 package com.rockthejvm.beginner.Functional
 
+import scala.util.{Failure, Random, Success, Try}
+
 object HandlingFailure extends App {
-  
+  val aSuccess = Success(3)
+  val aFailure = Failure(new RuntimeException("Failure"))
+
+  println(aSuccess)
+  println(aFailure)
+
+  def unsafeMethod(): String = throw new RuntimeException("NO STRING FOR YOU BUSTER")
+
+  //try objects via the apply method
+  val potentialFailure = Try(unsafeMethod())
+  println(potentialFailure)
+
+  //syntax sugar
+  val anotherPotentialFailure = Try {
+    //code that might throw fail
+  }
+
+  //utilities
+  println(potentialFailure.isSuccess)
+
+  //or else
+  def backupMethod(): String = "a valid result"
+  val fallbackTry = Try(unsafeMethod()).orElse(Try(backupMethod()))
+  println(fallbackTry)
+
+
+
+  val host = "localhost"
+  val port = "8080"
+  def renderHTML(page: String): Unit = println(page)
+
+  class Connection {
+    def get(url: String): String =  {
+      val random = new Random(System.nanoTime())
+      if (random.nextBoolean()) "<html> ... </html>"
+      else throw new RuntimeException("Connection Interrupted")
+    }
+    def getSafe(url: String): Try[String] = Try(get(url))
+  }
+
+  object HttpService {
+    val random = new Random(System.nanoTime())
+
+    def getConnection(host: String, port: String): Connection = {
+      if(random.nextBoolean()) new Connection
+      else throw new RuntimeException("connection could not be established")
+    }
+    def getSafeConnection(host: String, port: String): Try[Connection] = Try(getConnection(host, port))
+  }
+
+  //if you get the html page from the connection, print it to the console i.e. call renderHTML
+  val possibleConnection = HttpService.getSafeConnection(host, port)
+  val possibleHtml = possibleConnection.flatMap(connection => connection.getSafe("/home"))
+  possibleHtml.foreach(renderHTML)
+
+  //shorthand version
+  HttpService.getSafeConnection(host, port)
+    .flatMap(connection => connection.getSafe("/home"))
+    .foreach(renderHTML)
+
+  //with for-comprehension
+  for {
+    connection <- HttpService.getSafeConnection(host, port)
+    html <- connection.getSafe("/home")
+  }yield renderHTML(html)
 }
