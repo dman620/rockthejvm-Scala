8dd6fd8afcbfa7aa97128fe134fa9e87a9731beb
Mandl <Derek.B.Mandl@ehi.com>
2021-12-20 10:22:54 -0600

    I did some work on  dark syntax and advanced patterns

--- src/main/scala/com/rockthejvm/advanced/exercises/Lecture6.scala
+++ src/main/scala/com/rockthejvm/advanced/exercises/Lecture6.scala
@@ -14,67 +14,67 @@ object Lecture6 extends App {
   /* Exercise:
   *  devise custom pattern matching solution for these conditions
   */
-  object IsPrime{
-    def unapply(n: Int): Option[String] = {
-      if(n == 0) None
-      else if(n < 0) Some("Integer cannot be negative")
-      else Some("Your integer " + (if(isPrime(n)) "is prime." else "is not prime."))
-    }
+  object prime{
+    def unapply(n: Int): Boolean = isPrime(n)
   }
-  object IsEven{
-    def unapply(n: Int): Option[String] = {
-      if(n % 2 == 0) Some(s"${n} is even")
-      else Some(s"${n} is not even")
-    }
+  object even{
+    def unapply(n: Int): Boolean = n % 2 == 0
   }
 
   //function returns true if n is prime, otherwise false
   //if n <= 0 the function will return true
   def isPrime(n: Int): Boolean = {
     @tailrec
-    def go(n: Int, x: Int, accumulator: Boolean): Boolean = {
+    def go(num: Int, x: Int, accumulator: Boolean): Boolean = {
       if(x <= 1) accumulator
       else {
-        val divisible: Boolean = n % x == 0
-        go(n, x - 1, accumulator & !divisible)
+        val divisible: Boolean = num % x == 0
+        go(num, x - 1, accumulator & !divisible)
       }
     }
-    go(n, n - 1, accumulator = true)
+    //3 ways of accomplishing the same goal
+    val abs = if(n < 0) n * -1 else n
+    go(abs, abs - 1, accumulator = true)
+/*    if(n < 0)go(n * -1, (n * -1) - 1, accumulator = true)
+    else go(n, n - 1, accumulator = true)*/
+/*
+    go(if(n < 0) n * -1 else n, (if(n < 0)n * -1 else n) - 1, accumulator = true)
+*/
   }
 
-  val myNum = -2
+  val myNum = -7
   //requires the definition of Int.unapply above to work
   //this is the galaxy brain solution
   val primeStatus = myNum match{
-    case IsPrime(status) => s"Integer: $myNum \n" + status
-    case IsEven(status) => s"Integer: $myNum \n" + status
-    case _ => s"Unable to determine prime-ness of $myNum"
+    case prime() if (myNum != 0) => s"Integer: $myNum is a prime number!"
+    case even() => s"Integer: $myNum is an even number!"
+    case _ => s"Unable to determine properties of $myNum"
   }
   //does not use any supplemental code to work
   //this is the small brain solution
-  val primeStatus2 = isPrime(myNum) match{
+/*  val primeStatus2 = isPrime(myNum) match{
     case true => s"Your integer $myNum is prime"
     case false => s"Your integer $myNum is not prime"
-  }
+  }*/
   //also does not use any supplemental code to work
   //medium brain solution
-  val primeStatus3 = myNum match{
+/*  val primeStatus3 = myNum match{
     case x if x == 0 => s"Cannot determine the prime-ness of ${x}"
     case x if x < 0 => s"Cannot determine the prime-ness of negative number $x"
     case x if isPrime(x) => s"${x} is prime"
     case x if !isPrime(x) => s"${x} is not prime"
-  }
+  }*/
   println(primeStatus)
-  println(primeStatus2)
-  println(primeStatus3)
+  //println(primeStatus2)
+  //println(primeStatus3)
 
   //extra practice because i want to:
   //make a list with every prime up to max of n
 
-  val max = 10
+/*  val max = 10
   val listOfNPrimes = List.range(1, max).filter(isPrime)
   println(s"Every prime up to $max is:")
-  listOfNPrimes.foreach(x => println("-->" + x))
+  listOfNPrimes.foreach(x => println("-->" + x))*/
 
 
 }
--- src/main/scala/com/rockthejvm/advanced/lectures/part1/AdvancedPatternMatching.scala
+++ src/main/scala/com/rockthejvm/advanced/lectures/part1/AdvancedPatternMatching.scala
@@ -1,11 +1,15 @@
 package com.rockthejvm.advanced.lectures.part1
 
+import scala.annotation.tailrec
+import scala.collection.convert.ImplicitConversions.`collection asJava`
+
 object AdvancedPatternMatching extends App {
   val numbers = List(1)
-  val description = numbers match{
+  val description = numbers match {
     case head :: Nil => println(s"the only element is $head")
     case _ =>
   }
+
   /*
   * -constants
   * -wildcards
@@ -13,29 +17,106 @@ object AdvancedPatternMatching extends App {
   * -tuples
   * -some special magic, like above
   */
-  class Person(val name: String, val age: Int)//not a case class, so can't pattern match with it (no boilerplate map, flatmap, filter)
-  object PersonPattern{
+  class Person(val name: String, val age: Int) //not a case class, so can't pattern match with it (no boilerplate map, flatmap, filter)
+
+  object PersonPattern {
     def unapply(person: Person): Option[(String, Int)] = {
-      if(person.age < 21) Some(person.name -> person.age)
+      if (person.age < 21) Some(person.name -> person.age)
       else None
     }
+
     def unapply(age: Int): Option[String] = {
-      Some(if(age < 21) "minor" else "major")
+      Some(if (age < 21) "minor" else "major")
     }
   }
+
   val bob = new Person("Bob", 20)
-  val greeting = bob match{
+  val greeting = bob match {
     case PersonPattern(n, a) => s"Hi, my name is $n, and I am $a years old."
     case _ =>
   }
   println(greeting) //works though Person is not a case class
 
-  val legalStatus = bob.age match{
+  val legalStatus = bob.age match {
     case PersonPattern(status) => s"My Legal Status is $status"
     case _ =>
   }
   println(legalStatus)
 
+  //case head :: Nil => println(s"the only element is $head")
+  //this is an infix pattern, with the ::, we will do our own here
+  //INFIX PATTERNS
+  case class Or[A, B](a: A, b: B) //in scala this is called Either
+
+  val either = Or(2, "two")
+  val humanDescription = either match {
+    //case Or(number, string) => s"$number is written as $string"
+    //same as
+    case number Or string => s"$number is written as $string"
+  }
+  println(humanDescription)
+
+  //decomposing sequences
+  val vararg = numbers match {
+    case List(1, _*) => "starting with 1"
+  }
+  //the above pattern matches any list that starts with 1.  i.e. Ambiguous length
+  //how to decompose a list where there is an unknown number of elements to decompose?
+  abstract class MyList[+A] {
+    def head: A = ???
+
+    def tail: MyList[A] = ???
+  }
+
+  case object Empty extends MyList[Nothing]
+
+  case class Cons[+A](override val head: A, override val tail: MyList[A]) extends MyList[A]
+
+  object MyList{
+    def unapplySeq[A](list: MyList[A]): Option[Seq[A]] =
+      if(list == Empty) Some(Seq.empty)
+      else unapplySeq(list.tail).map(list.head +: _)
+  }
+  //in action:
+  val myList: MyList[Int] = Cons(1, Cons(2, Cons(3, Empty)))
+  println(myList match{
+    case MyList(1, 2, _*) => "starting with 1, 2"
+    case _ => "not starting with 1, 2"
+  })
+
+  //dereks practice
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def go(num: Int, x: Int, accumulator: Boolean): Boolean = {
+      if (x <= 1) accumulator
+      else {
+        val divisible: Boolean = num % x == 0
+        go(num, x - 1, accumulator & !divisible)
+      }
+    }
+
+    //3 ways of accomplishing the same goal
+    val abs = if (n < 0) n * -1 else n
+    go(abs, abs - 1, accumulator = true)
+    /*    if(n < 0)go(n * -1, (n * -1) - 1, accumulator = true)
+        else go(n, n - 1, accumulator = true)*/
+    /*
+        go(if(n < 0) n * -1 else n, (if(n < 0)n * -1 else n) - 1, accumulator = true)
+    */
+  }
+
+  def findThePrimes(nums: Int*): List[Int] = {
+    @tailrec
+    def go(thisList: Seq[Int], accumulator: List[Int]): List[Int] = {
+      if (thisList.isEmpty) accumulator
+      else go(thisList.tail, if (isPrime(thisList.head)) accumulator :+ thisList.head else accumulator)
+    }
+
+    go(nums, List[Int]())
+  }
+
+  findThePrimes(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 31, 53, 102, 314, 2223423, 234213, 1039, 1234567, 32432234,  314159)
+    .forEach(x => println(x + " is prime"))
 
 
 }
